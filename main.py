83d17c2c7f9af6b9bbcb98ed4d4aa6a066731182
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2023-02-26 23:46:31 -0300

    Extract images from pdf afip factura and decode

---
+++ main.py
@@ -0,0 +1,3 @@
+from extract_images_from_pdf_afip_factura_and_decode import extract_images_from_pdf_afip_factura_and_decode
+
+print(extract_images_from_pdf_afip_factura_and_decode('./sample_files/2000005044986390.pdf'))
